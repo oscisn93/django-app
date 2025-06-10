e454cf8bc0838a887ae448894c584ef6f6ab234d
big-o <big-o@nix.os>
2025-06-10 15:59:02 -0700

    tutorial complete

--- polls/models.py
+++ polls/models.py
@@ -1,9 +1,9 @@
 import datetime
 
 
-from django.db import models
 from django.utils import timezone
 from django.contrib import admin
+from django.db import models
 
 
 class Question(models.Model):
@@ -12,6 +12,7 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
+
     @admin.display(boolean=True, ordering="pub_date", description="Published recently?")
     def was_published_recently(self):
         now = timezone.now()
